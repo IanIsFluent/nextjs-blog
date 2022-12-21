7c81eab630ea1c333e17f47645f633bd0e6badfa
Ian Grainger <ian@isfluent.com>
2022-12-21 06:14:53 +0000

    add react-query and make a request to autocomplete all on the API

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,15 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Link from 'next/link';
+import { useState } from 'react';
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+  useQueryClient,
+} from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export default function Home() {
   return (
@@ -10,45 +19,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1 className={styles.title}>
-          Read <Link href="/posts/first-post">my first page!</Link>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+      <QueryClientProvider client={queryClient}>
+        <Main />
+      </QueryClientProvider>
 
       <footer>
         <a
@@ -113,4 +86,102 @@ export default function Home() {
       `}</style>
     </div>
   );
+
+  function Main() {
+    const [acAllValue, setAcAllValue] = useState('art');
+    const { isLoading, isError, data, isSuccess, error, refetch } = useQuery({
+      queryKey: ['acAll'],
+      enabled: false,
+      retry: false,
+      queryFn: () =>
+        fetch(
+          'https://become-propel-preview.azurewebsites.net/api/v1/autocomplete/all?q=' +
+            acAllValue
+        ),
+    });
+
+    return (
+      <main>
+        <h1 className={styles.title}>
+          Read <Link href="/posts/first-post">my first page!</Link>
+        </h1>
+
+        <p className={styles.description}>
+          Using propel API at
+          https://become-propel-preview.azurewebsites.net/swagger.
+        </p>
+
+        <div className={styles.grid}>
+          <div className={styles.card}>
+            <h3>Autocompletes</h3>
+            <form>
+              <label
+                htmlFor="all"
+                style={{
+                  marginRight: '.5em',
+                }}
+              >
+                All
+              </label>
+              <input
+                type="text"
+                id="all"
+                name="all"
+                style={{
+                  marginRight: '.5em',
+                }}
+                value={acAllValue}
+                onChange={(e) => setAcAllValue(e.target.value)}
+              ></input>
+              <button type="button" onClick={() => refetch()}>
+                Send
+              </button>
+            </form>
+          </div>
+          {/* <a href="https://nextjs.org/docs" className={styles.card}>
+      <h3>Documentation &rarr;</h3>
+      <p>Find in-depth information about Next.js features and API.</p>
+      </a> */}
+
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h3>Learn &rarr;</h3>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </a>
+
+          <a
+            href="https://github.com/vercel/next.js/tree/master/examples"
+            className={styles.card}
+          >
+            <h3>Examples &rarr;</h3>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </a>
+
+          <a
+            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+          >
+            <h3>Deploy &rarr;</h3>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
+        </div>
+        <h3>Results</h3>
+        <div
+          style={{
+            width: '100%',
+            backgroundColor: 'lightyellow',
+            padding: '1em',
+            fontFamily: 'monospace',
+          }}
+        >
+          <>
+            {isError && <div>Error: {(error as any)?.message}</div>}
+            {isLoading && <div>Loading...</div>}
+            {isSuccess && JSON.stringify(data, null, 2)}
+          </>
+        </div>
+      </main>
+    );
+  }
 }
