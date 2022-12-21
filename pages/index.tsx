7a74520412a9e9d1a37b3cc3c91afd4aca77e8d0
Ian Grainger <ian@isfluent.com>
2022-12-21 11:41:17 +0000

    create one big hook for all the functions

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import {
   useQuery,
 } from '@tanstack/react-query';
 import { Autocomplete } from '../components/autocomplete';
+import { useApiQueries } from '../hooks/api-queries';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -85,127 +86,24 @@ export default function Home() {
   function Main() {
     const propelWebUrl = 'https://become-propel-preview.azurewebsites.net';
     const apiBaseUrl = `${propelWebUrl}/api/v1`;
-
-    const [activeQueryName, setActiveQueryName] = useState('');
-
-    const [acAllValue, setAcAllValue] = useState('art');
-    const [acCoursesValue, setAcCoursesValue] = useState('art');
-    const [acInstitutionsValue, setAcInstitutionsValue] = useState('art');
-    const [acSubjectsValue, setAcSubjectsValue] = useState('art');
-    const [acPlacesValue, setAcPlacesValue] = useState('art');
-
-    const [instDetailsValue, setInstDetailsValue] = useState(
-      'the-university-of-cambridge'
-    );
-    const [instSearchValue, setInstSearchValue] = useState(
-      'subject=art&\nplacename=hull&\nradius=5'
-    );
-
-    const queries = {
-      acAll: useQuery({
-        queryKey: ['acAll'],
-        queryFn: async () => {
-          setActiveQueryName('acAll');
-          var res = await fetch(
-            `${apiBaseUrl}/autocomplete/all?q=${acAllValue}`
-          );
-          return res.json();
-        },
-      }),
-      acCourses: useQuery({
-        queryKey: ['acCourses'],
-        queryFn: async () => {
-          setActiveQueryName('acCourses');
-          var res = await fetch(
-            `${apiBaseUrl}/autocomplete/courses?q=${acCoursesValue}`
-          );
-          return res.json();
-        },
-      }),
-      acInstitutions: useQuery({
-        queryKey: ['acInstitutions'],
-        queryFn: async () => {
-          setActiveQueryName('acInstitutions');
-          var res = await fetch(
-            `${apiBaseUrl}/autocomplete/institutions?q=${acInstitutionsValue}`
-          );
-          return res.json();
-        },
-      }),
-      acSubjects: useQuery({
-        queryKey: ['acSubjects'],
-        queryFn: async () => {
-          setActiveQueryName('acSubjects');
-          var res = await fetch(
-            `${apiBaseUrl}/autocomplete/subjects?q=${acSubjectsValue}`
-          );
-          return res.json();
-        },
-      }),
-      acPlaces: useQuery({
-        queryKey: ['acPlaces'],
-        queryFn: async () => {
-          setActiveQueryName('acPlaces');
-          var res = await fetch(
-            `${apiBaseUrl}/autocomplete/places?q=${acPlacesValue}`
-          );
-          return res.json();
-        },
-      }),
-      instDetails: useQuery({
-        queryKey: ['instDetails'],
-        queryFn: async () => {
-          setActiveQueryName('instDetails');
-          var res = await fetch(
-            `${apiBaseUrl}/institutions/${instDetailsValue}`
-          );
-          if (res.status === 404) {
-            return 'not found';
-          }
-          return res.json();
-        },
-      }),
-      instSearch: useQuery({
-        queryKey: ['instSearch'],
-        queryFn: async () => {
-          setActiveQueryName('instSearch');
-          var res = await fetch(
-            `${apiBaseUrl}/institutions?${instSearchValue}`
-          );
-          return res.json();
-        },
-      }),
-      placesList: useQuery({
-        queryKey: ['placesList'],
-        queryFn: async () => {
-          setActiveQueryName('placesList');
-          var res = await fetch(`${apiBaseUrl}/places`);
-          return res.json();
-        },
-      }),
-    };
-    const latestQuery = queries[activeQueryName];
-    const resultText = useMemo(() => {
-      if (!latestQuery) {
-        return '';
-      }
-      if (latestQuery.isFetching) {
-        return 'Loading...';
-      }
-      if (latestQuery.isError) {
-        return `Error: ${latestQuery.error.message}`;
-      }
-      if (latestQuery.data) {
-        return JSON.stringify(latestQuery.data, null, 2);
-      }
-      return '';
-    }, [
-      activeQueryName,
-      latestQuery?.isFetching,
-      latestQuery?.data,
-      latestQuery?.isError,
-      latestQuery?.error,
-    ]);
+    const {
+      acAllValue,
+      setAcAllValue,
+      acCoursesValue,
+      setAcCoursesValue,
+      acInstitutionsValue,
+      setAcInstitutionsValue,
+      acSubjectsValue,
+      setAcSubjectsValue,
+      acPlacesValue,
+      setAcPlacesValue,
+      instDetailsValue,
+      setInstDetailsValue,
+      instSearchValue,
+      setInstSearchValue,
+      queries,
+      resultText,
+    } = useApiQueries(apiBaseUrl);
 
     return (
       <main>
